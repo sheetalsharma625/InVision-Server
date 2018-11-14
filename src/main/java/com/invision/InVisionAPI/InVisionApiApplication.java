7d687cf1c7275f17289c9b17a9bb4293192e42a1
Sheetal Sharma <sheetalsharma625@gmail.com>
2018-11-14 08:06:04 +0530

    Empty spring project

---
+++ src/main/java/com/invision/InVisionAPI/InVisionApiApplication.java
@@ -0,0 +1,12 @@
+package com.invision.InVisionAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InVisionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InVisionApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/invision/InVisionAPI/InVisionApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.invision.InVisionAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InVisionApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
