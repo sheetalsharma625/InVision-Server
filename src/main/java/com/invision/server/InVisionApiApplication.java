d00b31f8ebbc0696ccf023395b2e574012e6a7e0
Sheetal Sharma <sheetalsharma625@gmail.com>
2018-11-16 21:37:22 +0530

    Define Bean for RestTemplate

--- src/main/java/com/invision/server/InVisionApiApplication.java
+++ src/main/java/com/invision/server/InVisionApiApplication.java
@@ -2,11 +2,18 @@ package com.invision.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class InVisionApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InVisionApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(InVisionApiApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate provideRestTemplate() {
+        return new RestTemplate();
+    }
 }
